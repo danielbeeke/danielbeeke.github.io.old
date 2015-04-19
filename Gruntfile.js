d19c5bef2e4a5267bca241d9959dc0c7e65d3ae2
dbeeke <daniel@studiofonkel.nl>
2015-04-19 22:14:40 +0200

    switching branches 4

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,7 +249,7 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.min.js',
-            'CNAME'
+            '../CNAME'
             //'apple-touch*.png'
           ],
           dest: '<%= yeoman.dist %>'
