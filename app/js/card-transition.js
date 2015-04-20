2f85992726f78a5312ee3887d357c7e39960da30
dbeeke <daniel@studiofonkel.nl>
2015-04-20 21:54:00 +0200

    responsive styles 2

--- app/js/card-transition.js
+++ app/js/card-transition.js
@@ -7,12 +7,14 @@ $(function() {
     var transitionImage = $($('.blog-item-background', this)[0].outerHTML)
     transitionImage.addClass('transition-card')
     transitionImage.css({
-      width: $(that).width() * 1.36,
-      height: $(that).height() * 1.36,
-      top: $(that).offset().top - $(window).scrollTop() - 24,
-      left: $(that).offset().left - 47
+      width: $(that).parent().width() * 1.36,
+      height: $(that).parent().height() * 1.36,
+      top: $(that).offset().top - $(window).scrollTop() - 35,
+      left: $(that).offset().left - 55
     })
     $('body').append(transitionImage)
+
+
     setTimeout(function () {
       transitionImage.addClass('show')
     }, 100)
@@ -32,7 +34,7 @@ $(function() {
     $('.transition-card').one('transitionend', function () {
       $('.transition-card').css({
         left: newLeft,
-        top: newTitleHeight + (46 * 2) + 78,
+        top: newTitleHeight + (46 * 2) + 79,
         width: $('.fifth-grid-start-tester').width(),
         height: 400
       }).one('transitionend', function () {
