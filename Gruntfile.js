1cc451c05b42334903de198829914adc1e910646
dbeeke <daniel@studiofonkel.nl>
2015-04-21 20:02:26 +0200

    cname 4

--- Gruntfile.js
+++ Gruntfile.js
@@ -196,8 +196,8 @@ module.exports = function (grunt) {
         options: {
           collapseWhitespace: true,
           collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true
+          removeAttributeQuotes: false,
+          removeRedundantAttributes: false
         },
         files: [{
           expand: true,
