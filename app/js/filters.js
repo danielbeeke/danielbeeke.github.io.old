4cb35dc2e208d1a219cb8f2bf9c0dc63d83fbbae
dbeeke <daniel@studiofonkel.nl>
2015-04-21 12:22:52 +0200

    github api search

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,27 +4,61 @@ $(function() {
   $('.card-filters .tag').on('click', function () {
     $(this).toggleClass('active')
 
-    $('.card').parent().addClass('hidden').removeClass('visible')
-
     $('.card-overview').addClass('transition-in').addClass('transition-in-prepare')
 
     setTimeout(function () {
       setFilters()
-      setItemsInARowClass()
-      removeTransitionClasses()
     }, 300)
 
     return false
   })
 
   window.setFilters = function() {
+    var itemsToSetActiveByFilter = []
+    var itemsToSetActiveBySearch = []
+    var itemsToSetActive = []
+
+    $('.card').parent().addClass('hidden').removeClass('visible')
+
     if ($('.card-filters .tag.active').length) {
       $.each($('.card-filters .tag.active'), function (delta, category) {
-        $('.card.' + $(category).attr('data-tag')).parent().addClass('visible').removeClass('hidden')
+        $.each($('.card.' + $(category).attr('data-tag')), function (cardDelta, card) {
+          itemsToSetActiveByFilter.push(card)
+        })
       })
     }
     else {
-      $('.card').parent().removeClass('hidden').addClass('visible')
+      $.each($('.card'), function (cardDelta, card) {
+        itemsToSetActiveByFilter.push(card)
+      })
     }
+
+    if (window.searchResults && window.searchResults.length) {
+      $.each(window.searchResults, function (delta, id) {
+        itemsToSetActiveBySearch.push($('[data-id="' + id + '"]')[0])
+      })
+    }
+
+    if ($('.search-field.has-value').length) {
+      var arrays = [itemsToSetActiveByFilter, itemsToSetActiveBySearch]
+
+      itemsToSetActive = arrays.shift().reduce(function(res, v) {
+          if (res.indexOf(v) === -1 && arrays.every(function(a) {
+              return a.indexOf(v) !== -1
+          })) res.push(v)
+          return res
+      }, [])
+    }
+    else {
+      itemsToSetActive = itemsToSetActiveByFilter
+    }
+
+    $.each(itemsToSetActive, function (delta, item) {
+      $(item).parent().addClass('visible').removeClass('hidden')
+    })
+
+    setItemsInARowClass()
+    removeTransitionClasses()
   }
+
 });
---
+++ app/js/search.js
@@ -0,0 +1,68 @@
+$(function() {
+  'use strict';
+
+  $('.search-icon').on('click', function () {
+    $('.search-field').val('').change()
+  })
+
+  var debounce
+
+  $('.search-field').on('change keyup', function () {
+    var searchString = $('.search-field').val()
+    if (searchString) {
+      $('.search-field').addClass('has-value')
+    }
+    else {
+      $('.search-field').removeClass('has-value')
+    }
+
+    clearTimeout(debounce)
+
+    debounce = setTimeout(function () {
+      searchSite()
+    }, 700)
+  })
+
+  function searchSite() {
+    var searchString = $('.search-field').val()
+
+    $('.search-wrapper').addClass('is-searching')
+
+    var url = 'https://api.github.com/search/code?q=' + searchString + '+in:file+repo:danielbeeke/danielbeeke.github.io+extension:md'
+
+    $.ajax({
+      dataType: "json",
+      url: url,
+      headers: {
+        // This is used to go around the rate limit for anon users.
+        'Authorization': make_base_auth('searcherfor', 'thisaccountdoesnothing')
+      },
+      success: function (data) {
+        window.searchResults = []
+
+        $('.search-wrapper').removeClass('is-searching')
+
+        $.each(data.items, function (delta, item) {
+          var pathSplitted = item.path.split('/')
+
+          // We are only interested in posts.
+          if (pathSplitted[1] == '_posts') {
+            pathSplitted.splice(0, 1)
+            var id = pathSplitted.join('/')
+
+            window.searchResults.push(id)
+          }
+        })
+
+        setFilters()
+      }
+    })
+  }
+
+  function make_base_auth(user, password) {
+    var tok = user + ':' + password;
+    var hash = btoa(unescape(encodeURIComponent(tok)));
+    return "Basic " + hash;
+  }
+
+});
