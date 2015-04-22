6237342a6516aa372a93b54f0ad580fc10e5c3cb
dbeeke <daniel@studiofonkel.nl>
2015-04-22 12:10:26 +0200

    updates 2

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,12 +4,7 @@ $(function() {
   $('.card-filters .tag').on('click', function () {
     $(this).toggleClass('active')
     window.location.hash = ''
-    $('.card-overview').addClass('transition-in').addClass('transition-in-prepare')
-
-    setTimeout(function () {
-      setFilters()
-    }, 300)
-
+    setFilters()
     return false
   })
 
@@ -19,75 +14,80 @@ $(function() {
   })
 
   window.setFilters = function() {
-    var itemsToSetActiveByFilter = []
-    var itemsToSetActiveBySearch = []
-    var itemsToSetActive = []
-    var categories = []
+    $('.card-overview').addClass('transition-in').addClass('transition-in-prepare')
 
-    if (window.location.hash) {
-      var hash = window.location.hash.substr(1)
-      categories = hash.split('+')
+    setTimeout(function () {
+      var itemsToSetActiveByFilter = []
+      var itemsToSetActiveBySearch = []
+      var itemsToSetActive = []
+      var categories = []
 
-      $.each(categories, function (delta, category) {
-        $('[data-tag="' + category + '"]').addClass('active')
-        setTimeout(function () {
-          $('[data-tag="' + category + '"]').addClass('clicked')
-        }, 400)
-      })
-    }
+      if (window.location.hash) {
+        var hash = window.location.hash.substr(1)
+        categories = hash.split('+')
 
-    $('.card').parent().addClass('hidden').removeClass('visible')
-
-    if ($('.card-filters .tag.active').length) {
-      $.each($('.card-filters .tag.active'), function (delta, category) {
-        categories.push($(category).attr('data-tag'))
-      })
-    }
-
-    if (categories.length) {
-      $('.clear-filters').addClass('visible')
-      if (!window.location.hash) {
-        window.location.hash = categories.join('+')
+        $.each(categories, function (delta, category) {
+          $('[data-tag="' + category + '"]').addClass('active')
+          setTimeout(function () {
+            $('[data-tag="' + category + '"]').addClass('clicked')
+          }, 400)
+        })
       }
-      $.each(categories, function (delta, category) {
-        $.each($('.card.' + category), function (cardDelta, card) {
+
+      $('.card').parent().addClass('hidden').removeClass('visible')
+
+      if ($('.card-filters .tag.active').length) {
+        $.each($('.card-filters .tag.active'), function (delta, category) {
+          categories.push($(category).attr('data-tag'))
+        })
+      }
+
+      if (categories.length) {
+        $('.clear-filters').addClass('visible')
+        if (!window.location.hash) {
+          window.location.hash = categories.join('+')
+        }
+        $.each(categories, function (delta, category) {
+          $.each($('.card.' + category), function (cardDelta, card) {
+            itemsToSetActiveByFilter.push(card)
+          })
+        })
+      }
+      else {
+        $('.clear-filters').removeClass('visible')
+        $.each($('.card'), function (cardDelta, card) {
           itemsToSetActiveByFilter.push(card)
         })
+      }
+
+      if (window.searchResults && window.searchResults.length) {
+        $.each(window.searchResults, function (delta, id) {
+          itemsToSetActiveBySearch.push($('[data-id="' + id + '"]')[0])
+        })
+      }
+
+      if ($('.search-field.has-value').length) {
+        var arrays = [itemsToSetActiveByFilter, itemsToSetActiveBySearch]
+
+        itemsToSetActive = arrays.shift().reduce(function(res, v) {
+            if (res.indexOf(v) === -1 && arrays.every(function(a) {
+                return a.indexOf(v) !== -1
+            })) res.push(v)
+            return res
+        }, [])
+      }
+      else {
+        itemsToSetActive = itemsToSetActiveByFilter
+      }
+
+      $.each(itemsToSetActive, function (delta, item) {
+        $(item).parent().addClass('visible').removeClass('hidden')
       })
-    }
-    else {
-      $('.clear-filters').removeClass('visible')
-      $.each($('.card'), function (cardDelta, card) {
-        itemsToSetActiveByFilter.push(card)
-      })
-    }
 
-    if (window.searchResults && window.searchResults.length) {
-      $.each(window.searchResults, function (delta, id) {
-        itemsToSetActiveBySearch.push($('[data-id="' + id + '"]')[0])
-      })
-    }
+      setItemsInARowClass()
+      removeTransitionClasses()
+    }, 300)
 
-    if ($('.search-field.has-value').length) {
-      var arrays = [itemsToSetActiveByFilter, itemsToSetActiveBySearch]
-
-      itemsToSetActive = arrays.shift().reduce(function(res, v) {
-          if (res.indexOf(v) === -1 && arrays.every(function(a) {
-              return a.indexOf(v) !== -1
-          })) res.push(v)
-          return res
-      }, [])
-    }
-    else {
-      itemsToSetActive = itemsToSetActiveByFilter
-    }
-
-    $.each(itemsToSetActive, function (delta, item) {
-      $(item).parent().addClass('visible').removeClass('hidden')
-    })
-
-    setItemsInARowClass()
-    removeTransitionClasses()
   }
 
 });
